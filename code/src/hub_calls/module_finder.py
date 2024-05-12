25c37b9e9e51cf4cfe9698e3352e814ae595ee6a
Akanksha <Akanksha@macbook-pro-385.wifi.local.cmu.edu>
2024-05-12 17:17:08 -0400

    reformatting

--- code/src/hub_calls/module_finder.py
+++ code/src/hub_calls/module_finder.py
@@ -1,12 +1,15 @@
+import community as community_louvain
+import networkx as nx
 import numpy as np
 import pandas as pd
-import networkx as nx
-import community as community_louvain
 
-#implement spectral clustering
+# implement spectral clustering
+
 
 class Graph:
-    def __init__(self, edge_list=None, matrix=None, projection=None, sample="", parent=None):
+    def __init__(
+        self, edge_list=None, matrix=None, projection=None, sample="", parent=None
+    ):
         """
         Initializes a Graph object with an edge list or a connectivity matrix and a projection
         """
@@ -18,7 +21,9 @@ class Graph:
             self.connect = matrix
             self.projection = projection
         else:
-            raise ValueError("Either provide an edge list or a connectivity matrix with projection.")
+            raise ValueError(
+                "Either provide an edge list or a connectivity matrix with projection."
+            )
 
         self.parent = parent
         self.sample = sample
@@ -36,9 +41,9 @@ class Graph:
             print("Root Graph")
             if self.cell:
                 # Load interval table specific to the sample from a file
-                self.interval_table = pd.read_table(f"../Temp/{self.sample}/chrom_bin_index.txt", sep="\t")
+                self.interval_table = pd.read_table(
+                    f"../Temp/{self.sample}/chrom_bin_index.txt", sep="\t"
+                )
             else:
                 # If no sample specified, use an empty DataFrame
                 self.interval_table = pd.DataFrame()
-
-    
