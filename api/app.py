35385d9a80bb507b6b1f946d866af886da97089d
Akanksha <Akanksha@macbook-pro-385.wifi.local.cmu.edu>
2024-05-15 23:41:00 -0400

    dir changes

--- api/app.py
+++
@@ -1,8 +0,0 @@
-#Flash/Django web-API for the model
-
-from flask import Flask, request, jsonify
-#import custom modules from src.analysis_pipeline import Class1
-
-app = Flask(__name__)
-
-##code to define the API endpoints
