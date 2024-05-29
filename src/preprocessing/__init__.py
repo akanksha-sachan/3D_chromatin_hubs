2836dc528c94527c65c38fe24ba6bf2b7ebc83ed
Akanksha <Akanksha@macbook-pro-385.wifi.local.cmu.edu>
2024-05-29 13:26:47 -0400

    renamed module

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,5 +1,5 @@
 """__init__.py file for making relative imports smoother"""
 
 from .open_chromatin_processing import *
-from .edgelist_creator import *
+from .hic_graph import *
 from .genes_processing import *
